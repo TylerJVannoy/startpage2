fe7957fe725bb1b9de213e0754b9026a7d1cf325
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-12-20 20:34:34 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+  hour12: true,
+};
+
+const ICONS = [
+  {
+    icon: "ri-youtube-fill",
+    link: "https://www.youtube.com/",
+  },
+
+  {
+    icon: "ri-wheelchair-fill",
+    link: "https://www.4chan.org/",
+  },
+];
+
+const LIST_ITEMS = [
+  {
+    name: "/pol/",
+    link: "https://boards.4chan.org/pol/",
+  },
+];
